0b35575b0f3f87dcf9c1824af7b13603f7c0a060
Alex H <alex.holder@bigfootds.com>
2024-01-25 20:37:34 +1100

    fix: change userlist state value from fetch result

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       .then(response => response.json())
       .then(data => {
         console.log("Data from /users/ is: " + JSON.stringify(data));
-        setUserList(data);
+        setUserList(data.result);
       })
     }
 
